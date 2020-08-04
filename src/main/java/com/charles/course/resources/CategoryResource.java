3e608c1975b89a6d005e9b90c69343dedf402ba5
Charles Rodrigues <c.a.rodrigues-01@hotmail.com>
2020-08-04 20:57:50 -0300

    Category

---
+++ src/main/java/com/charles/course/resources/CategoryResource.java
@@ -0,0 +1,35 @@
+package com.charles.course.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.charles.course.entities.Category;
+import com.charles.course.services.CategoryService;
+
+@RestController
+@RequestMapping(value = "/Categories")
+public class CategoryResource {
+	
+	@Autowired
+	private CategoryService service;
+	
+	@GetMapping
+	public ResponseEntity<List<Category>> findAll() {
+		List<Category> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Category> findById(@PathVariable Long id){
+		Category obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+
+}
